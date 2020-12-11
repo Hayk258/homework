1290383c38f64c57227db00b38ab263c7ab533e8
Hayk258 <hayk-00@mail.ru>
2020-12-11 19:45:41 +0500

    homework

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # homework
 it is my homework 
+
+# Hayk
+I have more homeworks 
---
+++ homework.py
@@ -0,0 +1,29 @@
+# print('hellow world')
+# name = input ("what is your name ?")
+# lastname = input ("what's your last name ?")
+# mail = input ("what is your mail ?")
+# country = input ("what is your country ?")
+# addres = input ("what's your address ?")
+# index = input ("what is your index ?")
+# age=input ("how old are you ?")
+# yourgender = input ("what is your gender ?")
+# maritalstatus = input ("marital status ?")
+# car = input ("What do you drive ?")
+# carnumber = input ("number of your machine ?")
+# university = input ("where do you study ?")
+# educations = input ("your education ?")
+# department = input ("what is your Department ?")
+# profession = input ("what is your profession ?")
+
+# print('my name is',Hayk,"my lastname",Galoyan,
+# 	"my mail is",haykmail,"my country is ",Erevan,
+# 	"my addres is ",akievyan45,"my index is",afdf,"i am",28,"years old""my gender",murs,
+# 	"my maritalstatus is",jenat,"my car is",lexus,"my carnumber is",34gr343,"my university is",urfu,"my educations is",visshee,"my department is",rtf,"my profession is",informatika,)
+# age = input("how old are you ")
+# print(2020-int(age))
+
+
+
+print ("Python is powerful... and fast; \n \t plays well with others; \n runs everywhere;is friendly & easy to learn")
+
+

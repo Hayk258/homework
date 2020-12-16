e7a3ec165e258e8e0bd868b9ae73540cde9b96f2
Hayk258 <hayk-00@mail.ru>
2020-12-16 14:56:48 +0500

    def func

---
+++ homework21.py
@@ -0,0 +1,132 @@
+# def calculator(x,choise,y):
+# 	if choise == "+":
+# 		return x+y
+# 	elif choise == '-':
+# 		return x-y
+# 	elif choise =='*':
+# 		return x * y
+# 	elif choise == '/':
+# 		return x/y
+# x = int(input('num1: '))
+# choise = input('+ - * /')
+# y = int(input('num2: '))
+# print(calculator(x,choise,y))
+
+
+'''Write a python program
+ to find max of two
+ numbers'''
+
+# def max_of_two( x, y ):
+#     if x > y:
+#         return x
+#     return y
+
+# print(max_of_two(1,7))
+
+
+
+'''Write a python program
+ to sum all numbers'''
+
+
+# def sum_all(*kwards):
+# 	count = 0
+# 	for i in kwards:
+# 		count += i
+# 	print(count)
+# sum_all(1,2,3,4,5,6,7,8,9)
+
+
+
+'''Write a python program
+to multiply all numbers'''
+
+# def numbers(*kwards):
+# 	count = 1
+# 	for i in kwards:
+# 		count *= i
+# 	print(count)
+# numbers(1,12,13,4,5,6,7,8,9)
+
+'''Write a python program
+ to sum all letter and
+number in your string'''
+
+
+# def x(name):
+# 	count = 0
+# 	num = 0
+# 	for i in name:
+# 		if i.isalpha():
+# 			count+= 1
+# 		elif i.isdigit():
+# 			num += 1
+			
+# 	print(count,num)
+# name = 'hayk 1992'
+# x(name)	
+
+
+
+'''6.You are given a program that takes all 3
+passengers ages as inputs and inserts them in
+a list. Complete the program so that If it
+finds a value less than 16, it breaks the
+loop and outputs "Too young! ".
+If the age requirement is satisfied, the
+program outputs "Get ready!".'''
+
+
+# def pasajir(num1,num2,num3):
+# 	if num1 > 16 and num2 >16 and num3 > 16:
+# 		print('vi proxodite po vozrostu ')
+# 	else:
+# 		print('vi ne  proxodite po vozrostu ')
+# num1 = int(input('1)please enter your age '))
+# num2 = int(input('2)please enter your age '))
+# num3 = int(input('3)please enter your age '))
+# pasajir(num1,num2,num3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

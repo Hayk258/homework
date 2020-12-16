0b02e35498c24641764a5ccdddcfa0fb157a6e4a
Hayk258 <hayk-00@mail.ru>
2020-12-16 20:08:50 +0500

    homework22.py

---
+++ homework22.py
@@ -0,0 +1,123 @@
+'''1. km to mile
+Write a python function which
+conversion kilometer to miles.'''
+
+
+# def km_to_ml(km):
+# 	res = km/1.6
+# 	return res
+# km = int(input('km: '))
+# print(km_to_ml(km))
+
+
+'''2. Time
+Write a python function which
+conversion days to seconds.'''
+
+
+# def deys_second(deys):
+# 	res = deys * 24 * 60 * 60
+# 	return res
+# deys = int(input('days: '))
+# print(deys_second(deys),'seconds')
+
+
+
+'''3. Password
+Write a python function which
+generate a valid password.'''
+
+
+
+# def valid_password(password):
+# 	count = 0
+# 	char = 0
+# 	if len(password) >= 8:
+# 		for i in password:
+# 			if i.isdigit():
+# 				count += 1
+# 			elif i.isalpha():
+# 				char += 1
+# 		if count > 1 and char > 1:
+# 			return 'your password is strong '
+# 		else:
+# 			return 'your password is not strong '
+# 	else:
+# 		return 'your password length then 8 '
+# password = input('please enter password ')
+# print(valid_password(password))
+
+
+
+'''Factorial'''
+
+# import math
+# def fac(n):
+# 	res = math.factorial(n)
+# 	return res
+
+# n = int(input('factorial: '))
+# print(fac(n))
+
+
+'''Given an list of numbers.
+Find the maximum element in
+list.Without use max function'''
+
+
+
+# def max_num(num):
+# 	num.sort()
+# 	return num[-1]
+
+	
+# num = [1,66,7,8,33,99,87,1,2,3,4,5]
+# print(max_num(num))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

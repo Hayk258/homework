c4ef0040de86778177e004f0416037d7405ec873
Hayk258 <hayk-00@mail.ru>
2021-01-12 17:20:59 +0500

    'homework26'

---
+++ homework26.py
@@ -0,0 +1,61 @@
+import json
+
+# myinfo = {
+# 	'name':'Hayk',
+# 	'age':28,
+# 	'year':1992
+# }
+
+# myinfo1 = open('info.json','w')
+# json.dump(myinfo,myinfo1)
+
+# # myinfo = open('info.json')
+# # print(json.load(myinfo))
+
+
+
+'''Write a Python program to get info in JSON file
+about you.'''
+
+
+
+# my_info = 'about_me.json'
+# f = open(my_info,'w')
+# name = input('name: ')
+# age = input('age: ')
+# year = input('year: ')
+# res = '\nname '+ name + '\nage ' + age + '\nyear ' + year
+# json.dump(res,f)
+# f.close()
+
+
+
+'''Write a Python program to check if your json file
+have this info.'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
